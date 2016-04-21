7ec0a9bcd44d622c16c6a5cc34a6a40f4ec55e0f
ueda-koji-kakaku <ueda.koji.kakaku@gmail.com>
2016-04-21 04:25:25 +0000

    add message:age colum

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
 end
