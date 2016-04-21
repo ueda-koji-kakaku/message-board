bfa1dfc11b8d102440440d0a4a43f2d43c9c2675
ueda-koji-kakaku <ueda.koji.kakaku@gmail.com>
2016-04-21 02:15:42 +0000

    massege form is ok

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,4 +1,16 @@
 class MessageController < ApplicationController
   def index
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
   end
 end
